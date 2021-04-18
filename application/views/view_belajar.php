50df59c068246c143720c19f8d2e7542b0d3fdb5
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2021-04-18 11:40:00 +0700

    Add files via upload

---
+++ application/views/view_belajar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cara Membuat Heading dengan Helper HTML Codeigniter</title>
+</head>
+<body>
+	<?php 
+	echo heading("Helper HTML CodeIgniter",1);
+	echo heading("Helper HTML CodeIgniter",2);
+	echo heading("Helper HTML CodeIgniter",3);
+	echo heading("Helper HTML CodeIgniter",4);
+	echo heading("Helper HTML CodeIgniter",5);
+	echo heading("Helper HTML CodeIgniter",6);
+	?>
+</body>
+</html>
